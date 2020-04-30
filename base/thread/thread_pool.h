b55f27a3e6d652ecf9695f1b6bcbc2e3fcd7921a
kaydxh <523520531@qq.com>
2020-04-30 22:48:06 +0800

    [CHG] support  stop  and add test

--- base/thread/thread_pool.h
+++ base/thread/thread_pool.h
@@ -26,9 +26,24 @@ class ThreadPool : noncopyable {
 
   template <typename F, typename... Args>
   auto AddTask(F&& f, Args&&... args)
-      -> std::future<typename std::result_of<F(Args...)>::type>;
+      -> std::future<typename std::result_of<F(Args...)>::type> {
+    using return_type = typename std::result_of<F(Args...)>::type;
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    std::future<return_type> res = task->get_future();
+    {
+      std::unique_lock<std::mutex> lock(mutex_);
+      tasks_.emplace_back([task]() { (*task)(); });
+    }
+    condition_.notify_one();
+
+    return res;
+  }
+
   Task take();
   void loopInThread();
+  void stop();
 
  private:
   mutable std::mutex mutex_;
