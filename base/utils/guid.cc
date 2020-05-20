fd3081b38819115c84162e01a5b5c229ec90a34a
kayxhding <kayxhding@tencent.com>
2020-05-20 20:03:00 +0800

    [ADD] add guid

---
+++ base/utils/guid.cc
@@ -0,0 +1,29 @@
+//
+// Created by kayxhding on 2020-05-20 15:29:31
+//
+
+#include "guid.h"
+#include <iomanip>
+#include <sstream>
+#include "random/random.h"
+
+namespace kingfisher {
+namespace utils {
+
+std::string Guid::GenUuidString() {
+  uint64_t most_sign = kingfisher::random::Random::RandUInt64();
+  uint64_t least_sign = kingfisher::random::Random::RandUInt64();
+
+  std::stringstream stream;
+  stream << std::setfill('0') << std::hex << std::uppercase << std::setw(8)
+         << static_cast<uint32_t>(most_sign >> 32) << "-" << std::setw(4)
+         << static_cast<uint32_t>((most_sign >> 16) & 0x0000ffff) << "-"
+         << std::setw(4) << static_cast<uint32_t>(most_sign & 0x0000ffff) << "-"
+         << std::setw(4) << static_cast<uint32_t>(least_sign >> 48) << "-"
+         << std::setw(12) << (least_sign & 0x0000ffffffffffffULL);
+
+  return stream.str();
+}
+
+}  // namespace utils
+}  // namespace kingfisher
---
+++ test/base/test_guid.cc
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include "utils/guid.h"
+
+using namespace kingfisher;
+using namespace kingfisher::utils;
+
+class test_Guid : public testing::Test {
+ public:
+  test_Guid() {}
+  ~test_Guid() {}
+
+  virtual void SetUp(void) {}
+
+  virtual void TearDown(void) {}
+};
+
+TEST(test_Guid, Guid) {
+  std::string guid1;
+  std::string guid2;
+  for (auto i = 0; i < 100; ++i) {
+    guid1 = Guid::GenUuidString();
+    guid2 = Guid::GenUuidString();
+    std::cout << guid1 << " " << guid1.length() << std::endl;
+    std::cout << guid2 << " " << guid2.length() << std::endl;
+    EXPECT_NE(guid1, guid2);
+    EXPECT_EQ(guid1.length(), 36);
+    EXPECT_EQ(guid2.length(), 36);
+  }
+}
