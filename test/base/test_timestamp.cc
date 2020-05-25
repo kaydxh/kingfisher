583f71e57f25bb0ec2c695b3b63e07ad50b63080
kaydxh <523520531@qq.com>
2020-05-25 11:27:58 +0800

    [ADD] add test example

--- test/base/test_timestamp.cc
+++ test/base/test_timestamp.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <iostream>
 #include "time/timestamp.h"
 using namespace kingfisher;
 using namespace kingfisher::time;
@@ -23,9 +24,25 @@ TEST(test_Timestamp, All) {
   now.Swap(ts);
   ASSERT_FALSE(now.Valid());
   ASSERT_TRUE(ts.Valid());
+  std::cout << ts.ToString() << std::endl;
+  std::cout << ts.ToFormattedString() << std::endl;
+  std::cout << ts.ToSecDotMicroString() << std::endl;
 
   Timestamp tt(ts.MicroSecondsSinceEpoch());
   ASSERT_TRUE(tt == ts);
   ASSERT_TRUE(tt.ToString() == ts.ToString());
   ASSERT_TRUE(tt.ToFormattedString() == ts.ToFormattedString());
+  ASSERT_TRUE(tt.ToFormattedString(false) == ts.ToFormattedString(false));
+  ASSERT_TRUE(tt.ToSecDotMicroString() == ts.ToSecDotMicroString());
+
+  ASSERT_FALSE(ts < tt);
+  std::cout << DiffTimerence(ts, tt) << std::endl;
+  ASSERT_TRUE(0 == DiffTimerence(ts, tt));
+
+  Timestamp add_time = Timestamp::Invalid();
+  add_time = AddTime(ts, 1000.0);
+  std::cout << add_time.ToString() << std::endl;
+  std::cout << add_time.ToFormattedString() << std::endl;
+
+  ASSERT_TRUE(1000.0 == DiffTimerence(add_time, tt));
 }
