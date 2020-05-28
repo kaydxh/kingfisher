4c31544139ef2e146506fd658d515e12609df986
kaydxh <523520531@qq.com>
2020-05-28 20:53:38 +0800

    [CHG] complete build

--- base/file/file.h
+++ base/file/file.h
@@ -4,20 +4,26 @@
 #ifndef KINGFISHER_BASE_TIME_ZONE_H_
 #define KINGFISHER_BASE_TIME_ZONE_H_
 
+#include <endian.h>
+#include <stdint.h>
+#include <stdio.h>
+//#include <string.h>
+#include <stdexcept>
+
+#include <string>
+#include "core/noncopyable.hpp"
+
 namespace kingfisher {
 namespace file {
 
-#include <endian.h>
-#include <string>
-
-class File : kingfisher::core::noncopyable {
+class File : kingfisher::noncopyable {
  public:
   File(const char* file_name) : fp_(::fopen(file_name, "rb")) {}
 
   ~File() {
     if (fp_) {
       ::fclose(fp_);
-      fp = nullptr;
+      fp_ = nullptr;
     }
   }
 
@@ -37,24 +43,24 @@ class File : kingfisher::core::noncopyable {
     int32_t ret = 0;
     ssize_t bytes = ::fread(&ret, 1, sizeof(int32_t), fp_);
     if (sizeof(int32_t) != static_cast<int>(bytes)) {
-      throw std::login_error("bad read int32_t data");
+      throw std::logic_error("bad read int32_t data");
     }
 
     return static_cast<int32_t>(::be32toh(ret));
   }
 
-  int32_t ReadInt8() {
-    int8_t ret = 0;
-    ssize_t bytes = ::fread(&ret, 1, sizeof(int8_t), fp_);
-    if (sizeof(int8_t) != static_cast<int>(bytes)) {
-      throw std::login_error("bad read int8_t data");
+  int32_t ReadUInt8() {
+    uint8_t ret = 0;
+    ssize_t bytes = ::fread(&ret, 1, sizeof(uint8_t), fp_);
+    if (sizeof(uint8_t) != static_cast<int>(bytes)) {
+      throw std::logic_error("bad read uint8_t data");
     }
 
     return ret;
   }
 
  private:
-  File* fp_;
+  FILE* fp_;
 };
 }  // namespace file
 }  // namespace kingfisher
--- base/time/time_zone.h
+++ base/time/time_zone.h
@@ -5,6 +5,9 @@
 #ifndef KINGFISHER_BASE_TIME_TIMEZONE_H_
 #define KINGFISHER_BASE_TIME_TIMEZONE_H_
 
+#include <ctime>
+#include <memory>
+
 namespace kingfisher {
 namespace time {
 
@@ -20,19 +23,20 @@ class TimeZone {
     return static_cast<bool>(data_);
   }
 
-  struct tm ToLocalTime(time_t seconds_since_epoch) const;
+  struct tm ToLocalTime(std::time_t seconds_since_epoch) const;
 
   time_t FromLocalTime(const struct tm &t) const;
 
   // gmtime(3)
-  static struct tm ToUtcTime(time_t seconds_since_epoch, bool yday = false);
+  static struct tm ToUtcTime(std::time_t seconds_since_epoch,
+                             bool yday = false);
 
   // timegm(3)
-  static time_t FromUtcTime(const struct tm &t);
+  static std::time_t FromUtcTime(const struct tm &t);
 
   // year in [1900..2500], month in [1..12], day in [1..31]
-  static time_t FromUtcTime(int year, int month, int day, int hour, int minute,
-                            int seconds);
+  static std::time_t FromUtcTime(int year, int month, int day, int hour,
+                                 int minute, int seconds);
 
   struct Data;
 
