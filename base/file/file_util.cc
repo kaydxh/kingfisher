8b1b1019c0508b06cbaab85cfab74b007b118b51
kayxhding <kayxhding@tencent.com>
2020-06-08 17:08:12 +0800

    [ADD] add open file implementation

--- base/file/file_util.cc
+++ base/file/file_util.cc
@@ -3,6 +3,11 @@
 namespace kingfisher {
 namespace fileutil {
 
+int Open(const char *filename, int flags /* = O_RDWR | O_LARGEFILE | O_CREAT*/,
+         mode_t mode /* = 0666 */) {
+  return detail::WrapFuncT(open, filename, flags, mode);
+}
+
 ssize_t ReadFull(int fd, void *buf, size_t length) {
   return detail::WrapFileOpFuncT(read, fd, buf, length);
 }
