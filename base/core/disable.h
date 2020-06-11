73b64fe5bd392c54e1296f2f49323bfd46aeb20e
kaydxh <523520531@qq.com>
2020-06-11 17:38:37 +0800

    [ADD] add disable macros

---
+++ base/core/disable.h
@@ -0,0 +1,24 @@
+//
+// Created by kayxhding on 2020-06-11 17:22:13
+//
+
+#ifndef KINNGFISHER_BASE_CORE_DISABLE_H_
+#define KINNGFISHER_BASE_CORE_DISABLE_H_
+
+#define DISABLE_COPY(ClassName) ClassName(const ClassName &) = delete
+
+#define DISABLE_ASSIGN(ClassName) \
+  ClassName &operator=(const ClassName &) = delete
+
+#define DISABLE_COPY_AND_ASSIGN(ClassName &) \
+  DISABLE_COPY(ClassName);                   \
+  DISABLE_ASSIGN(ClassName)
+
+#define DISABLE_IMPLICIT_CONSTRUCTORS(ClassName) \
+  ClassName() = delete;                          \
+  DISABLE_COPY_AND_ASSIGN(ClassName)
+
+template <typename T>
+inline void ignore_result(const T &) {}
+
+#endif
