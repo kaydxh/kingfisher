6764003b0a81fbf17e435a22a976077d3b530e1f
kayxhding <kayxhding@tencent.com>
2020-06-15 17:18:08 +0800

    [CHG] run loop in thread

--- base/time/timer.h
+++ base/time/timer.h
@@ -5,8 +5,11 @@
 #ifndef KINNGFISHER_BASE_CORE_TIMER_H_
 #define KINNGFISHER_BASE_CORE_TIMER_H_
 
+#include <iostream>
 #include <limits>
+//#include <thread>
 #include "core/disable.h"
+#include "thread/thread_pool.h"
 
 namespace kingfisher {
 namespace time {
@@ -15,6 +18,9 @@ class TimerWheelSlot;
 class TimerWheel;
 
 using Tick = uint64_t;
+
+static uint32_t getJiffies();
+void msleep(uint32_t msecs);
 
 class TimerEventBase {
  public:
@@ -88,20 +94,26 @@ class TimerWheelSlot {
 
 class TimerWheel {
  public:
-  TimerWheel(Tick now = 0) : now_(now), ticks_pending_(0) {}
-  ~TimerWheel() {}
+  TimerWheel(Tick now = 0) : now_(getJiffies()), ticks_pending_(0) {}
+  ~TimerWheel() { thread_pool_.stop(); }
 
+  void Start();
   bool advance(Tick interval,
                size_t max_events = std::numeric_limits<size_t>::max());
 
   // a repeating timeout event that will fire every "interval" time
   void schedule(TimerEventBase* event, Tick interval);
 
+  void Join();
+
  private:
+  void runLoop();
   bool process_current_slot(Tick now, size_t max_execute);
 
  private:
   static const int WIDTH_BITS = 8;
+  static const int NUM_LEVELS = (64 + WIDTH_BITS - 1) / WIDTH_BITS;
+  static const int MAX_LEVELS = NUM_LEVELS - 1;
   // 256
   static const int NUM_SLOTS = 1 << WIDTH_BITS;
 
@@ -110,6 +122,11 @@ class TimerWheel {
   Tick now_;
   Tick ticks_pending_;
   TimerWheelSlot slots_[NUM_SLOTS];
+
+  kingfisher::thread::ThreadPool thread_pool_;
+
+  // std::thread thread_;
+  bool running_ = true;
 };
 
 // implementation
@@ -160,7 +177,25 @@ void TimerEventBase::cancel() {
   relink(nullptr);
 }
 
+void TimerWheel::Start() {
+  thread_pool_.start();
+  thread_pool_.AddTask(&TimerWheel::runLoop, this);
+}
+
+void TimerWheel::runLoop() {
+  while (running_) {
+    //  Tick now = ++now_;
+    Tick now = now_;
+    if (!process_current_slot(now, std::numeric_limits<size_t>::max())) {
+    }
+    msleep(1);
+  }
+}
+
+void TimerWheel::Join() { thread_pool_.join(); }
+
 bool TimerWheel::advance(Tick interval, size_t max_events) {
+  return true;
   if (ticks_pending_) {
     ticks_pending_ += interval;
     Tick now = now_;
@@ -173,6 +208,7 @@ bool TimerWheel::advance(Tick interval, size_t max_events) {
   }
 
   while (interval--) {
+    msleep(1);
     Tick now = ++now_;
     if (!process_current_slot(now, max_events)) {
       ticks_pending_ = (interval + 1);
@@ -184,6 +220,7 @@ bool TimerWheel::advance(Tick interval, size_t max_events) {
 }
 
 bool TimerWheel::process_current_slot(Tick now, size_t max_execute) {
+  now = getJiffies() - now_;
   size_t slot_index = now & MASK;
   auto slot = &slots_[slot_index];
   while (slot->events()) {
@@ -198,15 +235,32 @@ bool TimerWheel::process_current_slot(Tick now, size_t max_execute) {
 }
 
 void TimerWheel::schedule(TimerEventBase* event, Tick interval) {
-  event->set_scheduled_at(now_ + interval);
+  auto diff = getJiffies() - now_;
+  std::cout << "diff: " << diff << std::endl;
+  event->set_scheduled_at(diff + interval);
   while (interval >= NUM_SLOTS) {
     interval = (interval + (now_ & MASK)) >> WIDTH_BITS;
   }
 
-  size_t slot_index = (now_ + interval) & MASK;
+  size_t slot_index = (diff + interval) & MASK;
   auto slot = &slots_[slot_index];
   event->relink(slot);
 }
+
+static uint32_t getJiffies() {
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return (ts.tv_sec * 1000 + ts.tv_nsec / 1e6);
+}
+
+void msleep(uint32_t msecs) {
+  struct timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = msecs * 1000;
+  select(0, nullptr, nullptr, nullptr, &tv);
+  //  std::cout << "msleep" << msecs << std::endl;
+}
+
 }  // namespace time
 }  // namespace kingfisher
 #endif
