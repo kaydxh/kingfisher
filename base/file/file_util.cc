57801119fc52fa98db4afd518ec230501ed7f710
kaydxh <523520531@qq.com>
2020-06-19 21:54:19 +0800

    [ADD] add copy file func

--- base/file/file_util.cc
+++ base/file/file_util.cc
@@ -1,6 +1,8 @@
 #include "file_util.h"
 #include <sys/file.h>
 
+#include "core/scope_guard.h"
+
 namespace kingfisher {
 namespace fileutil {
 
@@ -15,6 +17,43 @@ ssize_t ReadFull(int fd, void *buf, size_t length) {
 
 ssize_t WriteFull(int fd, const void *buf, size_t length) {
   return detail::WrapFileOpFuncT(write, fd, const_cast<void *>(buf), length);
+}
+
+bool CopyFile(const char *from_path, const char *to_path) {
+  ssize_t in_fd = -1;
+  ssize_t out_fd = -1;
+
+  SCOPE_EXIT {
+    if (in_fd != -1) {
+      close(in_fd);
+    }
+    if (out_fd != -1) {
+      close(out_fd);
+    }
+  };
+
+  in_fd = Open(from_path, O_RDONLY);
+  if (-1 == in_fd) {
+    return false;
+  }
+
+  out_fd = Open(to_path);
+  if (-1 == out_fd) {
+    return false;
+  }
+
+  const size_t BUFFER_SIZE = 1024;
+  char buf[BUFFER_SIZE] = {'\0'};
+
+  int sz = 0;
+  while ((sz = ReadFull(in_fd, buf, BUFFER_SIZE)) > 0) {
+    if (sz != WriteFull(out_fd, buf, sz)) {
+      return false;
+    }
+    std::cout << "----WriteFull: " << sz << std::endl;
+  }
+
+  return true;
 }
 
 bool DeleteFile(const char *filename) {
