9f612bbe1b07f5b33f14f2e06482217663c769f3
xiaohua ding <xiao_hua_ding@sina.cn>
2021-05-22 13:42:48 +0800

    feat(time): add time counter

---
+++ base/time/time_counter.cc
@@ -0,0 +1,66 @@
+
+#include "time/time_counter.h"
+
+#include <cstdio>
+#include <functional>
+
+#include "time/timestamp.h"
+
+namespace kingfisher {
+namespace time {
+
+TimeCounter::TimeCounter(bool effect) : effect_(effect) { Tick("start"); }
+
+TimeCounter::~TimeCounter() {}
+
+void TimeCounter::Tick(const std::string &msg) {
+  if (effect_) {
+    starts_.emplace_back(Timestamp::Now());
+    messages_.emplace_back(msg);
+  }
+}
+
+std::string TimeCounter::String() {
+  if (!effect_) {
+    return "";
+  }
+
+  auto f = [&](int idx, std::string msg, double cost,
+               std::string at) -> std::string {
+    char buf[1024];
+    snprintf(buf, sizeof(buf), "#%d, msg: %s, cost: %f, at %s\n", idx,
+             msg.c_str(), cost, at.c_str());
+    return std::string(buf);
+  };
+
+  return summary(f);
+}
+
+std::string TimeCounter::summary(
+    std::function<std::string(int, std::string, double, std::string)> const
+        &f) {
+  if (!effect_) {
+    return "";
+  }
+
+  if (messages_.size() < starts_.size()) {
+    return "";
+  }
+
+  std::string result;
+  for (std::vector<Timestamp>::size_type i = 1; i < starts_.size(); ++i) {
+    result += f(i, messages_[i], DiffTimerence(starts_[i], starts_[i - 1]),
+                starts_[i].ToFormattedString());
+  }
+
+  return result;
+}
+
+void TimeCounter::Reset() {
+  starts_.clear();
+  messages_.clear();
+}
+
+}  // namespace time
+}  // namespace kingfisher
+
---
+++ base/time/time_counter.h
@@ -0,0 +1,40 @@
+
+#ifndef KINGFISHER_BASE_TIME_TIMECOUNTER_H_
+#define KINGFISHER_BASE_TIME_TIMECOUZONE_H_
+
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "time/timestamp.h"
+
+namespace kingfisher {
+namespace time {
+
+class Timestamp;
+
+class TimeCounter {
+ public:
+  TimeCounter(bool effect = true);
+  ~TimeCounter();
+
+  void Tick(const std::string &msg);
+  std::string String();
+  void Reset();
+
+ private:
+  std::string summary(std::function<std::string(int, std::string, double,
+                                                std::string)> const &f);
+
+ private:
+  // std::vector<uint64_t> starts_;
+  std::vector<Timestamp> starts_;
+  std::vector<std::string> messages_;
+  bool effect_;
+};
+
+}  // namespace time
+}  // namespace kingfisher
+
+#endif
+
