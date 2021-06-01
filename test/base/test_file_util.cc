87283d07798fb9ffc93c24b33786bfa98b069405
xiaohua ding <xiao_hua_ding@sina.cn>
2021-06-01 17:30:08 +0800

    test(file): test for make dir

--- test/base/test_file_util.cc
+++ test/base/test_file_util.cc
@@ -4,6 +4,7 @@
 
 #include "core/scope_guard.h"
 #include "file/file.h"
+#include "file/file_util.h"
 
 using namespace kingfisher;
 using namespace kingfisher ::fileutil;
@@ -26,3 +27,19 @@ TEST_F(test_FileUtil, CopyFile) {
 TEST_F(test_FileUtil, DeleteFile) { EXPECT_TRUE(DeleteFile("./hosts")); }
 
 TEST_F(test_FileUtil, IsDir) { EXPECT_TRUE(IsDir("/etc")); }
+
+TEST_F(test_FileUtil, WriteFile) {
+  std::string filename = "./tmp/test/meta.test";
+  for (int i = 0; i < 5; ++i) {
+    std::string content = "hello word\n";
+    auto n = WriteFile(filename, content, true);
+    std::cout << n << std::endl;
+    EXPECT_EQ(n, content.length());
+  }
+}
+
+TEST_F(test_FileUtil, MakeDirAll) {
+  std::string dir = "./tmp/";
+  int ret = MakeDirAll(dir);
+  EXPECT_EQ(ret, 0);
+}
