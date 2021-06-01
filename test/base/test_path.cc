292a976120c179f8b31803d520d74700a1422a94
kaydxh <523520531@qq.com>
2021-06-01 16:24:41 +0800

    test(path): test for split path

--- test/base/test_path.cc
+++ test/base/test_path.cc
@@ -18,8 +18,24 @@ class test_Path : public testing::Test {
 };
 
 TEST_F(test_Path, Abs) {
-  std::string filepath = "../../.";
+  std::string filepath = ".";
   std::string abs_path = Abs(filepath);
-
+  ASSERT_TRUE(IsAbs(abs_path));
   std::cout << "absolute path: " << abs_path << std::endl;
+}
+
+TEST_F(test_Path, IsAbs) {
+  std::string filepath = ".";
+  ASSERT_FALSE(IsAbs(filepath));
+}
+
+TEST_F(test_Path, Split) {
+  std::string filepath = "/data/home/image/1.jpg";
+  std::string dir;
+  std::string filename;
+  int ret = Split(filepath, dir, filename);
+  ASSERT_TRUE(ret == 0);
+  ASSERT_TRUE(dir == "/data/home/image/");
+  ASSERT_TRUE(filename == "1.jpg");
+  std::cout << "dir: " << dir << ", file:" << filename << std::endl;
 }
