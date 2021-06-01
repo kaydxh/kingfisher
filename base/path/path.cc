5bd5812ad410811a7cfd42926a7716c5bacfcde8
kaydxh <523520531@qq.com>
2021-06-01 16:24:10 +0800

    feat(path): split path

--- base/path/path.cc
+++ base/path/path.cc
@@ -16,5 +16,18 @@ bool IsAbs(const std::string &path) {
   return path.length() > 0 && path[0] == '/';
 }
 
+std::string Dir(const std::string &path) { return ""; }
+
+int Split(const std::string &path, std::string &dir_return_,
+          std::string &file_return_) {
+  auto pos = path.find_last_of("/");
+  if (pos != std::string::npos) {
+    dir_return_ = path.substr(0, pos + 1);
+    file_return_ = path.substr(pos + 1);
+    return 0;
+  }
+
+  return -1;
+}
 }  // namespace path
 }  // namespace kingfisher
