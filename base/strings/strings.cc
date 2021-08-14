eccd9758afa03c3fa9ca2b7b507e63343c9c89fc
kayxhding <kayxhding@tencent.com>
2021-08-14 17:23:05 +0800

    feat(string): isnumber for string

--- base/strings/strings.cc
+++ base/strings/strings.cc
@@ -1,10 +1,21 @@
 
 #include "strings/strings.h"
 
+#include <cctype>
 #include <sstream>
 
 namespace kingfisher {
 namespace strings {
+
+bool IsNumber(const std::string &s) {
+  for (char const &c : s) {
+    if (!std::isdigit(c)) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 std::vector<std::string> Split(const std::string &s, const char delim) {
   std::string word;
