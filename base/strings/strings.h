fa007309035f069091bfbfbfa31151cb7b1100aa
kaydxh <kayxhding@tencent.com>
2021-08-16 22:13:44 +0800

    feat(string): has prefix

--- base/strings/strings.h
+++ base/strings/strings.h
@@ -16,6 +16,9 @@ int ParseUint64(uint64_t &result, const std::string &s, int base = 10);
 
 std::vector<std::string> Split(const std::string &s, const char delim);
 
+bool HasPrefix(const std::string &s, const std::string &prefix,
+               bool case_sensitive = true);
+
 }  // namespace strings
 }  // namespace kingfisher
 #endif
