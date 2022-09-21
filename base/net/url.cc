9e6692d68362bff17295d52f1f6a6380300ed7d4
kayxhding <kayxhding@tencent.com>
2022-09-21 13:17:37 +0800

    feat(url): add url decode

--- base/net/url.cc
+++ base/net/url.cc
@@ -1,4 +1,5 @@
 #include "url.h"
+#include "crypto/hex.h"
 
 namespace kingfisher {
 namespace net {
@@ -6,7 +7,7 @@ namespace net {
 std::string URLEncode(const std::string &source) {
   std::ostringstream result;
   for (auto i = source.begin(); i != source.end(); ++i) {
-    if (isalnum(*i) || *i == '-' || *i == '_' || *i == '~') {
+    if (isalnum(*i) || *i == '-' || *i == '_' || *i == '.' || *i == '~') {
       result << *i;
     } else if (*i == ' ') {
       result << '+';
@@ -15,6 +16,28 @@ std::string URLEncode(const std::string &source) {
       result << '%';
       result << kingfisher::crypto::ToHex(*i >> 4);
       result << kingfisher::crypto::ToHex(*i & 0xf);
+    }
+  }
+
+  return result.str();
+}
+
+std::string URLDecode(const std::string &source) {
+  std::ostringstream result;
+  for (size_t i = 0; i < source.size(); ++i) {
+    if (source[i] == '%') {
+      if (i < source.size() - 2) {
+        if (source[i + 1] && source[i + 2]) {
+          result << static_cast<char>(
+                        (kingfisher::crypto::FromHex(source[i + 1]) << 4 |
+                         kingfisher::crypto::FromHex(source[i + 2])));
+          i += 2;
+        }
+      }
+    } else if (source[i] == '+') {
+      result << ' ';
+    } else {
+      result << source[i];
     }
   }
 
