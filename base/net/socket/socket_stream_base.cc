7c1c578e55f769f5f7631ffcf8b3fb8e434c5029
kayxhding <kayxhding@tencent.com>
2022-09-23 17:28:54 +0800

    feat(net): add stream buf

---
+++ base/net/socket/socket_stream_base.cc
@@ -0,0 +1,39 @@
+#include "socket_stream_base.h"
+
+// sockaddr_in
+#include <netinet/in.h>
+// inet_ntop
+#include <arpa/inet.h>
+
+// memset
+#include <cstring>
+
+namespace kingfisher {
+namespace net {
+
+SocketStreamBase::SocketStreamBase(size_t buf_size)
+    : std::iostream(NULL), buf_size_(buf_size) {}
+
+SocketStreamBase::~SocketStreamBase() {}
+
+void SocketStreamBase::NewRdbuf(SocketStreamBase* buf) {
+  // rdbuf(buf);
+}
+
+bool SocketStreamBase::GetRemoteHost(char* ip, size_t size, int* port) {
+  struct sockaddr_in addr;
+  socklen_t slen = sizeof(addr);
+  memset(&addr, 0, sizeof(addr));
+
+  int ret = getpeername(SocketFd(), (struct sockaddr*)&addr, &slen);
+  if (ret == 0) {
+    inet_ntop(AF_INET, &addr, ip, size);
+    if (NULL != port) {
+      *port = ntohs(addr.sin_port);
+    }
+  }
+
+  return ret == 0;
+}
+}
+}
