017008f5a3c59ad9897f9d16d9cdb6aefbde4a97
kaydxh <xiao_hua_ding@sina.cn>
2023-06-12 16:39:28 +0800

    refactor(script): generate cpp proto use third party proto bin

--- proto.gen.sh
+++ proto.gen.sh
@@ -10,5 +10,5 @@ set -o xtrace
 
 mkdir -p ./scripts
 bash -c "curl -s -L -H 'Cache-Control: no-cache' -o ./pkg/cmake/FindProtobuf.cmake https://raw.githubusercontent.com/kaydxh/golang/main/pkg/gocv/cgo/cmake/FindProtobuf.cmake"
-bash -c "curl -s -L -H 'Cache-Control: no-cache' -o ./scripts/proto-gen.sh https://raw.githubusercontent.com/kaydxh/golang/main/script/go_proto_gen.sh"
-bash scripts/proto-gen.sh -I .  --proto_file_path ./pkg  --with-cpp 
+#bash -c "curl -s -L -H 'Cache-Control: no-cache' -o ./scripts/proto-gen.sh https://raw.githubusercontent.com/kaydxh/golang/main/script/go_proto_gen.sh"
+#bash scripts/proto-gen.sh -I .  --proto_file_path ./pkg  --with-cpp 
