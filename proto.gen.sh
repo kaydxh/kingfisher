d41c1a299e5c9e7e45284eabe1ed485a8df2ea53
kaydxh <xiao_hua_ding@sina.cn>
2023-06-14 17:02:09 +0800

    refactor(script): add comment for generate cpp proto

--- proto.gen.sh
+++ proto.gen.sh
@@ -9,6 +9,9 @@ set -o pipefail
 set -o xtrace
 
 mkdir -p ./scripts
+# 编译cpp proto使用third party中的protoc 版本
 bash -c "curl -s -L -H 'Cache-Control: no-cache' -o ./pkg/cmake/FindProtobuf.cmake https://raw.githubusercontent.com/kaydxh/golang/main/pkg/gocv/cgo/cmake/FindProtobuf.cmake"
+
+# 编译go可以使用系统安装的protoc
 #bash -c "curl -s -L -H 'Cache-Control: no-cache' -o ./scripts/proto-gen.sh https://raw.githubusercontent.com/kaydxh/golang/main/script/go_proto_gen.sh"
 #bash scripts/proto-gen.sh -I .  --proto_file_path ./pkg  --with-cpp 
