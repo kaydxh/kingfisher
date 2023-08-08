418b6565d9adbdae4b094010e501b1793c88d990
kaydxh <523520531@qq.com>
2023-08-08 19:26:33 +0800

    feat(webserver): init

---
+++ pkg/webserver/webserverbase.h
@@ -0,0 +1,23 @@
+#ifndef KINGFISHER_PKG_WEB_SERVER_BASE_H_
+#define KINGFISHER_PKG_WEB_SERVER_BASE_H_
+
+#include <string>
+
+namespace kingfisher {
+namespace web {
+
+    class WebServerBase {
+    public:
+        WebServerBase() {};
+        virtual ~WebServerBase() {};
+        virtual std::string ServerName() = 0;
+
+        virtual void Run();
+        virtual void BeforrRun(void *args);
+        virtual void AfterRun(void *args);
+    };
+
+}
+}
+
+#endif
