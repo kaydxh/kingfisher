34c4ebb14c3ce1339d517007585fc08b63ca37c2
kaydxh <xiao_hua_ding@sina.cn>
2023-09-08 16:34:20 +0800

    feat(log): add glog

---
+++ pkg/log/config.cc
@@ -0,0 +1,42 @@
+#include "config.h"
+
+#include <memory>
+
+#include "config/yaml/yaml.h"
+
+namespace kingfisher {
+namespace log {
+
+int Config::LoadYaml() {
+  if (options.node != nullptr) {
+    return kingfisher::yaml::UnmarshalProtoMessage(*options.node, proto);
+  }
+
+  return 0;
+}
+
+std::shared_ptr<Config> Config::NewConfig(const ConfigOptions& opts) {
+  std::shared_ptr<Config> config = std::make_shared<Config>();
+  config->options = opts;
+
+  if (config->LoadYaml() != 0) {
+    return nullptr;
+  }
+
+  return config;
+}
+
+std::shared_ptr<CompletedConfig> Config::Complete() {
+  int completed_ret = LoadYaml();
+  std::shared_ptr<CompletedConfig> completed_config =
+      std::make_shared<CompletedConfig>(this, completed_ret);
+  return completed_config;
+}
+
+CompletedConfig::CompletedConfig(const Config* config, int completed_ret)
+    : config_(const_cast<Config*>(config)), completed_ret_(completed_ret) {}
+
+int CompletedConfig::Install() { return 0; }
+
+}  // namespace log
+}  // namespace kingfisher
---
+++ pkg/log/config.h
@@ -0,0 +1,43 @@
+#ifndef KINGFISHER_PKG_LOG_CONFIG_H_
+#define KINGFISHER_PKG_LOG_CONFIG_H_
+
+#include <yaml-cpp/yaml.h>
+
+#include "log.pb.h"
+
+namespace kingfisher {
+namespace log {
+
+class CompletedConfig;
+
+struct ConfigOptions {
+  YAML::Node* node = nullptr;
+};
+
+class Config {
+ public:
+  static std::shared_ptr<Config> NewConfig(const ConfigOptions& opts);
+  int LoadYaml();
+  std::shared_ptr<CompletedConfig> Complete();
+
+ private:
+  api::v1::viper::logs::Log proto;
+  ConfigOptions options;
+};
+
+class CompletedConfig {
+ public:
+  CompletedConfig(const Config* config, int completed_ret);
+  ~CompletedConfig() {}
+
+  int Install();
+
+ private:
+  Config* config_ = nullptr;
+  int completed_ret_ = 0;
+};
+
+}  // namespace log
+}  // namespace kingfisher
+
+#endif
