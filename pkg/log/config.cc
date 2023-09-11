9e5f4b73b41e19f23e26fb5bf41233902ff18d10
kaydxh <523520531@qq.com>
2023-09-11 18:14:11 +0800

    feat(log): debug for installing log

--- pkg/log/config.cc
+++ pkg/log/config.cc
@@ -3,6 +3,7 @@
 #include <memory>
 
 #include "config/yaml/yaml.h"
+#include "core/singleton.hpp"
 
 namespace kingfisher {
 namespace log {
@@ -15,27 +16,37 @@ int Config::LoadYaml() {
   return 0;
 }
 
-std::shared_ptr<Config> Config::NewConfig(const ConfigOptions& opts) {
-  std::shared_ptr<Config> config = std::make_shared<Config>();
-  config->options_ = opts;
+Config& Config::NewConfig(const ConfigOptions& opts) {
+  auto& config = kingfisher::core::Singleton<Config>::Instance();
+  config.options_ = opts;
 
-  if (config->LoadYaml() != 0) {
-    return nullptr;
+  if (config.LoadYaml() != 0) {
+    return config;
   }
 
   return config;
 }
 
-std::shared_ptr<CompletedConfig> Config::Complete() {
+CompletedConfig& Config::Complete() {
   int completed_ret = LoadYaml();
-  std::cout << "log:" << proto_.DebugString() << std::endl;
-  std::shared_ptr<CompletedConfig> completed_config =
-      std::make_shared<CompletedConfig>(this, completed_ret);
+  auto& completed_config =
+      kingfisher::core::Singleton<CompletedConfig>::Instance();
+  completed_config.Init(this, completed_ret);
   return completed_config;
 }
 
-CompletedConfig::CompletedConfig(const Config* config, int completed_ret)
-    : config_(config), completed_ret_(completed_ret) {}
+CompletedConfig::CompletedConfig() {}
+
+CompletedConfig::~CompletedConfig() {
+  if (completed_ret_ == 0) {
+    google::ShutdownGoogleLogging();
+  }
+}
+
+void CompletedConfig::Init(const Config* config, int completed_ret) {
+  config_ = config;
+  completed_ret_ = completed_ret;
+}
 
 int CompletedConfig::Apply() {
   if (completed_ret_ != 0) {
@@ -45,7 +56,42 @@ int CompletedConfig::Apply() {
   return Install();
 }
 
-int CompletedConfig::Install() { return 0; }
+int CompletedConfig::Install() {
+  google::InitGoogleLogging(config_->options_.app.c_str());
+
+  auto& proto = config_->proto_.log();
+  using namespace api::v1::viper::logs;
+  FLAGS_log_dir = proto.filepath();
+  FLAGS_max_log_size = proto.rotate_size();
+
+  switch (proto.level()) {
+    case Log_Level::Log_Level_trace:
+    case Log_Level::Log_Level_debug:
+    case Log_Level::Log_Level_info: {
+      FLAGS_minloglevel = google::INFO;
+      break;
+    }
+    case Log_Level::Log_Level_warn: {
+      FLAGS_minloglevel = google::WARNING;
+      break;
+    }
+    case Log_Level::Log_Level_error: {
+      FLAGS_minloglevel = google::ERROR;
+      break;
+    }
+    case Log_Level::Log_Level_fatal:
+    case Log_Level::Log_Level_panic: {
+      FLAGS_minloglevel = google::FATAL;
+      break;
+    }
+
+    default:
+      FLAGS_minloglevel = google::INFO;
+      break;
+  }
+
+  return 0;
+}
 
 }  // namespace log
 }  // namespace kingfisher
