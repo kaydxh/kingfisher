1eae6f4a018cca655b9f5f6ee96cffcafbcd5df8
kaydxh <xiao_hua_ding@sina.cn>
2023-09-15 11:05:02 +0800

    feat(path): base path

--- base/path/path.cc
+++ base/path/path.cc
@@ -1,5 +1,7 @@
 #include "path/path.h"
 
+#include <regex.h>
+
 namespace kingfisher {
 namespace path {
 
@@ -37,5 +39,74 @@ int Split(const std::string &path, std::string &dir_return_,
 
   return -1;
 }
+
+bool IsPathBeginSeparator(const std::string &path) {
+  if (path.empty()) {
+    return false;
+  }
+
+  char c = path[0];
+  return c == '/' || c == '\\';
+}
+
+bool IsPathEndSeparator(const std::string &path) {
+  if (path.empty()) {
+    return false;
+  }
+
+  char c = path[path.size() - 1];
+  return c == '/' || c == '\\';
+}
+
+std::string Join(const std::string &base, const std::string &path) {
+  if (base.empty()) {
+    return path;
+  }
+  if (path.empty()) {
+    return base;
+  }
+
+  std::string result;
+  bool base_path_end_sep = IsPathEndSeparator(base);
+  bool path_begin_sep = IsPathBeginSeparator(path);
+
+  if (base_path_end_sep && path_begin_sep) {
+    result = base + path.substr(1);
+  } else if (!base_path_end_sep && !path_begin_sep) {
+    result = base + "/" + path;
+  } else {
+    result = base + path;
+  }
+
+  return result;
+}
+
+int LastSlash(std::string &s) {
+  int i = s.size() - 1;
+  while (i >= 0 && s[i] != '/') {
+    i--;
+  }
+  return i;
+}
+
+std::string Base(const std::string &path) {
+  if (path.empty()) {
+    return ".";
+  }
+
+  std::string temp_path = path;
+  for (; temp_path.size() > 0 && temp_path[temp_path.size() - 1] == '/';) {
+    temp_path = temp_path.substr(0, temp_path.size() - 1);
+  }
+  int i = LastSlash(temp_path);
+  if (i >= 0) {
+    temp_path = temp_path.substr(i + 1);
+  }
+  if (temp_path.empty()) {
+    return "/";
+  }
+  return temp_path;
+}
+
 }  // namespace path
 }  // namespace kingfisher
--- base/path/path.h
+++ base/path/path.h
@@ -15,6 +15,16 @@ std::string Dir(const std::string &path);
 int Split(const std::string &path, std::string &dir_return_,
           std::string &file_return_);
 
+std::string Join(const std::string &base, const std::string &path);
+
+bool IsPathBeginSeparator(const std::string &path);
+bool IsPathEndSeparator(const std::string &path);
+
+// Base returns the last element of path.
+std::string Base(const std::string &path);
+
+int LastSlash(std::string &s);
+
 }  // namespace path
 }  // namespace kingfisher
 
