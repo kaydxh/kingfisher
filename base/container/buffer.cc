22947e929b845cee583f84cb51c41e091adccd0c
kaydxh <xiao_hua_ding@sina.cn>
2023-09-18 11:22:12 +0800

    feat(container): add buffer

---
+++ base/container/buffer.cc
@@ -0,0 +1,32 @@
+#include "buffer.h"
+
+#include <algorithm>
+#include <cstring>
+
+namespace kingfisher {
+namespace container {
+
+size_t Buffer::ReadableBytes() const { return writer_index_ - reader_index_; }
+
+size_t Buffer::WriteableBytes() const { return buffer_.size() - writer_index_; }
+
+const char* Buffer::Peek() const { return begin() + reader_index_; }
+
+char* Buffer::BeginWrite() { return begin() + writer_index_; }
+
+const char* Buffer::BeginWrite() const { return begin() + writer_index_; }
+
+void Buffer::Append(const std::string& data) {
+  ensureWriteSize(data.size());
+  std::copy(data.c_str(), data.c_str() + data.size(), BeginWrite());
+  // std::memcpy(BeginWrite(), data.c_str(), data.size());
+}
+
+void Buffer::ensureWriteSize(size_t len) {
+  if (WriteableBytes() < len) {
+    buffer_.resize(writer_index_ + len);
+  }
+}
+
+}  // namespace container
+}  // namespace kingfisher
