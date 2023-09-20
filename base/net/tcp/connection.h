1d19d3577775603d4a26def59a3236de1213a35f
kaydxh <523520531@qq.com>
2023-09-20 11:19:25 +0800

    feat(connection): add callback

--- base/net/tcp/connection.h
+++ base/net/tcp/connection.h
@@ -7,8 +7,10 @@
 #include <string>
 
 #include "core/noncopyable.hpp"
+#include "net/event/callback.h"
 #include "net/event/channel.h"
 #include "net/event/event_loop.h"
+#include "net/socket/net.buffer.h"
 #include "net/socket/socket.h"
 #include "net/socket/socket_addr.h"
 #include "time/timestamp.h"
@@ -20,10 +22,17 @@ class TcpConnection : public noncopyable_::noncopyable,
  public:
   TcpConnection(EventLoop* loop, const std::string& name, int sockfd,
                 const sockets::SockAddress& local_addr,
-                const sockets::SockAddress& peer_addr);
+                const sockets::SockAddress& peer_addr, bool keep_alive = true);
+
+  void Send(const void* data, size_t len);
 
  private:
-  void HandleRead(time::Timestamp recv_time);
+  void handleRead();
+  void handleWrite();
+  void handleClose();
+  void handleError();
+
+  void sendInLoop(const void* data, size_t len);
 
  private:
   EventLoop* loop_;
@@ -31,6 +40,12 @@ class TcpConnection : public noncopyable_::noncopyable,
   std::unique_ptr<Channel> channel_;
   const sockets::SockAddress local_addr_;
   const sockets::SockAddress peer_addr_;
+
+  MessageCallback message_cb_;
+  CloseCallback closeCallback_;
+
+  NetBuffer input_buffer_;
+  NetBuffer output_buffer_;
 };
 
 }  // namespace net
