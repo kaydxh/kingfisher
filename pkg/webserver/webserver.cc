5f97104ac365565af005b8eff6947f95afd8bae0
kaydxh <xiao_hua_ding@sina.cn>
2023-10-08 11:36:09 +0800

    build(webserver): build for webserver

--- pkg/webserver/webserver.cc
+++ pkg/webserver/webserver.cc
@@ -1,10 +1,17 @@
 #include "webserver.h"
 
+#include "log/config.h"
+
 namespace kingfisher {
 namespace web {
 
 WebServer::WebServer() {}
 WebServer::~WebServer() {}
+
+int WebServer::Init(const std::string& port) {
+  port_ = port;
+  return 0;
+}
 
 WebServer& WebServer::AddServiceOrDie(google::protobuf::Service* service) {
   int ret = server_.AddService(service, brpc::SERVER_DOESNT_OWN_SERVICE);
@@ -13,6 +20,18 @@ WebServer& WebServer::AddServiceOrDie(google::protobuf::Service* service) {
   }
 
   return *this;
+}
+
+int WebServer::Run() {
+  LOG(INFO) << "install server on " << port_;
+  int ret = server_.Start(port_.c_str(), &options_);
+  if (ret != 0) {
+    LOG(ERROR) << "failec to run server on " << port_ << ", ret: " << ret;
+    return ret;
+  }
+
+  server_.RunUntilAskedToQuit();
+  return 0;
 }
 
 }  // namespace web
---
+++ test/pkg/test_webserver.cc
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <string>
+
+#include "config/yaml/yaml.h"
+#include "core/scope_guard.h"
+#include "webserver/config.h"
+#include "webserver/webserver.h"
+#include "webserver/webserver.pb.h"
+
+class test_Webserver : public testing::Test {
+ public:
+  test_Webserver() {}
+  ~test_Webserver() {}
+
+  virtual void SetUp(void) {}
+
+  virtual void TearDown(void) {}
+};
+
+TEST_F(test_Webserver, ALL) {
+  std::string path = "./testdata/webserver.yaml";
+  YAML::Node yaml_data = kingfisher::yaml::GetYaml(path);
+  kingfisher::web::ConfigOptions opts;
+  opts.node = &yaml_data;
+  auto &ws = kingfisher::web::Config::NewConfig(opts).Complete().ApplyOrDie();
+  ws.Init("0.0.0.0:10000");
+  ws.Run();
+}
+
