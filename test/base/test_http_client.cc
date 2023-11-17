92d2f8466a88944f69dc885f1b885afbd1486c10
kaydxh <523520531@qq.com>
2023-11-17 14:04:22 +0800

    test(test): test http client

---
+++ test/base/test_http_client.cc
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include "net/http/http_client.h"
+#include "net/http/http_request.h"
+#include "net/http/http_response.h"
+
+using namespace kingfisher;
+using namespace kingfisher::net;
+
+class test_HttpClient : public testing::Test {
+ public:
+  test_HttpClient() {}
+  ~test_HttpClient() {}
+
+  virtual void SetUp(void) {}
+
+  virtual void TearDown(void) {}
+};
+
+TEST(test_HttpClient, HttpClient) {
+  HttpClient client;
+  HttpRequest req;
+  req.SetUrl("http://127.0.0.1:18000/Now");
+
+  HttpResponse resp;
+  int ret = client.Get(req, resp);
+  std::cout << "get ret: " << ret << ", body: " << resp.Body() << std::endl;
+  EXPECT_EQ(ret, 0);
+}
