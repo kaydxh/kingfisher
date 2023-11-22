602dd5c8d3c6efdfce441c1cbd8db7aad16afdb4
kaydxh <xiao_hua_ding@sina.cn>
2023-11-22 14:13:46 +0800

    refactor(file-transfer): refactor upload

--- pkg/file-transfer/ft.transfer.cc
+++ pkg/file-transfer/ft.transfer.cc
@@ -6,6 +6,7 @@
 #include "net/http/http_response.h"
 #include "strings/strings.h"
 #include "time/timer.h"
+#include "time/wait.h"
 
 namespace kingfisher {
 namespace file {
@@ -19,49 +20,50 @@ int FileTransfer::Init(const FileTransferOptions& opts) {
 }
 
 int FileTransfer::Download(std::string& data, const std::string& url) {
-  net::HttpClient client;
-  client.SetTimeoutMs(opts_.download_timeout_ms);
-  net::HttpRequest req;
-  req.SetUrl(url);
+  return time::Retry(
+      [&]() -> int {
+        net::HttpClient client;
+        client.SetTimeoutMs(opts_.download_timeout_ms);
+        net::HttpRequest req;
+        req.SetUrl(url);
 
-  net::HttpResponse resp;
+        net::HttpResponse resp;
 
-  int ret = 0;
-  for (int i = 0; i < opts_.retry_times; ++i) {
-    ret = client.Get(req, resp);
-    if (ret == 0) {
-      break;
-    }
-    LOG(ERROR) << strings::FormatString("failed to download %s, ret: %d",
-                                        url.c_str(), ret);
-    time::MsSleep(opts_.retry_interval);
-  }
-
-  if (ret == 0) {
-    data = resp.Body();
-  }
-
-  return ret;
+        int ret = 0;
+        ret = client.Get(req, resp);
+        if (ret != 0) {
+          LOG(ERROR) << strings::FormatString("failed to download %s, ret: %d",
+                                              url.c_str(), ret);
+          return ret;
+        }
+        data = resp.Body();
+        return 0;
+      },
+      opts_.retry_interval, opts_.retry_times);
 }
 
 int FileTransfer::Upload(const std::string& data, const std::string& url) {
-  net::HttpClient client;
-  client.SetTimeoutMs(opts_.upload_timeout_ms);
-  net::HttpRequest req;
-  req.SetUrl(url);
-  req.SetBody(data);
+  return time::Retry(
+      [&]() -> int {
+        net::HttpClient client;
+        client.SetTimeoutMs(opts_.upload_timeout_ms);
+        net::HttpRequest req;
+        req.SetUrl(url);
+        req.SetBody(data);
 
-  net::HttpResponse resp;
-  int ret = client.Put(req, resp);
-  if (ret != 0) {
-    LOG(ERROR) << strings::FormatString(
-        "failed to upload len %d to %s, ret: %d", data.size(), url.c_str(),
-        ret);
+        net::HttpResponse resp;
+        int ret = client.Put(req, resp);
+        if (ret != 0) {
+          LOG(ERROR) << strings::FormatString(
+              "failed to upload len %d to %s, ret: %d", data.size(),
+              url.c_str(), ret);
 
-    return ret;
-  }
+          return ret;
+        }
 
-  return 0;
+        return 0;
+      },
+      opts_.retry_interval, opts_.retry_times);
 }
 
 }  // namespace file
