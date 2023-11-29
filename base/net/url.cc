0061d077573b84c9a08aa5a339a9130cc49379f7
kaydxh <523520531@qq.com>
2023-11-29 19:50:43 +0800

    feat(uri): add uri

--- base/net/url.cc
+++ base/net/url.cc
@@ -1,5 +1,7 @@
 #include "url.h"
+
 #include "crypto/hex.h"
+#include "strings/strings.h"
 
 namespace kingfisher {
 namespace net {
@@ -12,7 +14,6 @@ std::string URLEncode(const std::string &source) {
     } else if (*i == ' ') {
       result << '+';
     } else {
-
       result << '%';
       result << kingfisher::crypto::ToHex(*i >> 4);
       result << kingfisher::crypto::ToHex(*i & 0xf);
@@ -29,8 +30,8 @@ std::string URLDecode(const std::string &source) {
       if (i < source.size() - 2) {
         if (source[i + 1] && source[i + 2]) {
           result << static_cast<char>(
-                        (kingfisher::crypto::FromHex(source[i + 1]) << 4 |
-                         kingfisher::crypto::FromHex(source[i + 2])));
+              (kingfisher::crypto::FromHex(source[i + 1]) << 4 |
+               kingfisher::crypto::FromHex(source[i + 2])));
           i += 2;
         }
       }
@@ -43,5 +44,35 @@ std::string URLDecode(const std::string &source) {
 
   return result.str();
 }
+
+URI::URI() {}
+URI::~URI() {}
+
+std::string URI::PercentDecode(const std::string &s) {
+  if (s.empty()) {
+    return s;
+  }
+  if (!strings::Contains(s, "%")) {
+    return s;
+  }
+
+  std::string out;
+  std::string unescaped;
+  out.reserve(s.size());
+  for (size_t i = 0; i < s.length(); i++) {
+    unescaped = "";
+    if (s[i] == '%' && i + 3 <= s.length() &&
+        strings::Unescape(unescaped, std::string("\\x") + s.substr(i + 1, 2),
+                          false) &&
+        unescaped.length() == 1) {
+      out += unescaped[0];
+      i += 2;
+    } else {
+      out += s[i];
+    }
+  }
+  return out;
 }
-}
+
+}  // namespace net
+}  // namespace kingfisher
