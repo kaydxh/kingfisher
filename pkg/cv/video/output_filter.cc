ce35b63926dc9490b4943a537cab69e876b2b91d
kaydxh <xiao_hua_ding@sina.cn>
2024-03-22 17:03:09 +0800

    feat(video): add output filter

--- pkg/cv/video/output_filter.cc
+++ pkg/cv/video/output_filter.cc
@@ -3,6 +3,7 @@
 #include "ffmpeg_error.h"
 #include "ffmpeg_filter.h"
 #include "ffmpeg_utils.h"
+#include "scripts/pack/ffmpeg/include/libavutil/frame.h"
 #include "stream.h"
 
 extern "C" {
@@ -23,7 +24,11 @@ namespace cv {
 
 OutputFilter::OutputFilter(const std::shared_ptr<FilterGraph> &fg,
                            const std::weak_ptr<Stream> &ost)
-    : av_class_(&output_filter_class), graph_(fg), ost_(ost) {}
+    : av_class_(&output_filter_class),
+      graph_(fg),
+      ost_(ost),
+      filtered_frame_(std::shared_ptr<AVFrame>(
+          av_frame_alloc(), [](AVFrame *frame) { av_frame_free(&frame); })) {}
 
 OutputFilter::~OutputFilter() {}
 
@@ -123,7 +128,73 @@ int OutputFilter::configure_output_video_filter(AVFilterInOut *out) {
   return 0;
 }
 
-int OutputFilter::configure_output_audio_filter(AVFilterInOut *in) { return 0; }
+int OutputFilter::configure_output_audio_filter(AVFilterInOut *out) {
+  auto const &ost = ost_.lock();
+  if (!ost) {
+    return AVERROR_STREAM_NOT_FOUND;
+  }
+
+  int ret = 0;
+  if (ost->codec_ctx_->codec_type != AVMEDIA_TYPE_AUDIO) {
+    av_log(this, AV_LOG_ERROR,
+           "Cannot connect audio filter to non audio input\n");
+    ret = AVERROR(EINVAL);
+    return ret;
+  }
+
+  const auto &codec_ctx = ost->codec_ctx_;
+  if (codec_ctx->ch_layout.order == AV_CHANNEL_ORDER_UNSPEC) {
+    av_channel_layout_default(&codec_ctx->ch_layout,
+                              codec_ctx->ch_layout.nb_channels);
+  }
+
+  AVFilterContext *last_filter = nullptr;
+  char name[255];
+  snprintf(name, sizeof(name), "abuffersink_out_%d_%d", ost->file_index_,
+           ost->stream_index_);
+
+  last_filter = out->filter_ctx;
+  int last_pad_idx = out->pad_idx;
+  if ((ret = FilterGraph::insert_filter(&last_filter, &last_pad_idx,
+                                        "abuffersink", nullptr)) < 0) {
+    av_log(this, AV_LOG_ERROR, "Cannot create filter %s: %s\n", name,
+           av_err2str(ret));
+    return ret;
+  }
+  if ((ret = av_opt_set_int(last_filter, "all_channel_counts", 1,
+                            AV_OPT_SEARCH_CHILDREN)) < 0) {
+    return ret;
+  }
+  filter_ = last_filter;
+
+  return 0;
+}
+
+int OutputFilter::reap_filters() {
+  const auto &filtered_frame = filtered_frame_.get();
+  while (filter_) {
+    int ret = av_buffersink_get_frame_flags(filter_, filtered_frame,
+                                            AV_BUFFERSINK_FLAG_NO_REQUEST);
+    if (ret < 0) {
+      if (ret != AVERROR(EAGAIN) && ret != AVERROR_EOF) {
+        av_log(this, AV_LOG_WARNING,
+               "Error in av_buffersink_get_frame_flags(): %s\n",
+               av_err2str(ret));
+      } else {
+        ret = 0;
+      }
+      return ret;
+    }
+
+    if (filtered_frame->pts != AV_NOPTS_VALUE) {
+      AVRational tb = av_buffersink_get_time_base(filter_);
+      filtered_frame->time_base = tb;
+    }
+
+    av_frame_unref(filtered_frame);
+  }
+  return 0;
+}
 
 }  // namespace cv
 }  // namespace kingfisher
