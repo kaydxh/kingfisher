26491e013bee9cb6c541ebcdb3e3d12ffd5ae2a8
kaydxh <xiao_hua_ding@sina.cn>
2024-04-09 14:55:40 +0800

    feat(video): init_output_stream_streamcopy

--- pkg/cv/video/ffmpeg_error.h
+++ pkg/cv/video/ffmpeg_error.h
@@ -30,4 +30,14 @@ inline char *av_ts2timestring(int64_t ts, AVRational *tb) {
 #define av_ts2timestr(ts, tb) av_ts2timestring(ts, tb)
 #endif
 
+#ifdef av_ts2str
+#undef av_ts2str
+
+inline std::string av_ts2string(int errnum) {
+  char str[AV_TS_MAX_STRING_SIZE]{0};
+  return av_ts_make_string(str, errnum);
+}
+#define av_ts2str(err) av_ts2string(err).c_str()
+#endif
+
 #endif
