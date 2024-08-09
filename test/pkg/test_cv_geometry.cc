bb851a5f0605622e9673b9c792ef3cff5904a5c3
kaydxh <xiao_hua_ding@sina.cn>
2024-08-09 16:27:32 +0800

    test(test_cv_geometry): test_cv_geometry

--- test/pkg/test_cv_geometry.cc
+++ test/pkg/test_cv_geometry.cc
@@ -37,3 +37,17 @@ TEST_F(test_Geometry, RotatePoint) {
 
   LOG(INFO) << message;
 }
+
+TEST_F(test_Geometry, RotateRect) {
+  cv::Size origin_size(720, 568);
+  cv::Rect origin_rect(36, 28, 100, 150);
+  cv::Rect rotated_rect;
+  int angle = 90;
+  Geometry::RotateRect(origin_size, origin_rect, angle, rotated_rect);
+
+  auto message = strings::FormatString(
+      "rotated rect: x=%d, y=%d, width=%d, height=%d", rotated_rect.x,
+      rotated_rect.y, rotated_rect.width, rotated_rect.height);
+
+  LOG(INFO) << message;
+}
